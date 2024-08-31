c854174790fa3259fd89c1d2f78ae3780c2cd9a0
Shinde Yashraj Pramod <145414813+yashrajshinde1000@users.noreply.github.com>
2024-08-31 11:03:51 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+ScrollReveal().reveal('.line',{delay:200});
+
+
+ScrollReveal({ distance: '60px' , delay:300} );
+ScrollReveal().reveal('.header', { origin: 'right' });
+ScrollReveal().reveal('.summary', { origin: 'left' });
+ScrollReveal().reveal('.education', { origin: 'right' });
+ScrollReveal().reveal('.experience', { origin: 'left' });
+ScrollReveal().reveal('.skill', { origin: 'right' });
+ScrollReveal().reveal('.project', { origin: 'left' });
+ScrollReveal().reveal('.contactme', { origin: 'right' });
+ScrollReveal().reveal('.form-container', { origin: 'right' });
+ScrollReveal().reveal('.head', { origin: 'left' });
+
+
